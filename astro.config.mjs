0d37b9519998dafda04a435772bf63ef10e1abf8
Edoardo <rossi.edoardo369@gmail.com>
2026-07-26 11:45:40 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import { defineConfig } from 'astro/config';
 // lascia base: '/' e site con l'URL della user page.
 export default defineConfig({
   site: 'https://rossiedoardo369.github.io',
-  base: '/portfolio-edoardo-rossi-2',
+base: '/portfolio-edoardo-rossi-2/',
 });
